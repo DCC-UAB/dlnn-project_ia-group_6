143b3ea32c9f2c34580ffeff43eb3bd1915f4917
nora826 <114749719+nora826@users.noreply.github.com>
2023-05-30 02:09:33 +0200

    Add files via upload

---
+++ s.py
@@ -0,0 +1,20 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+class final_dataset(Dataset):
+    def __init__(self, data, labels, transform=None):
+        self.data = data
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        sample = self.data[index]
+        label = self.labels[index]
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample, label
